680d6e90d16e320c158a8c0870dd614e9956dc88
myles1221 <myles.lewis@knights.ucf.edu>
2020-11-19 11:29:20 -0500

    second commit

---
+++ Lewis_p4.5/src/TaskItemTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+public class TaskItemTest {
+
+    @Test
+    public void creatingTaskItemFailsWithInvalidDueDate(){
+
+    }
+    @Test
+    public void creatingTaskItemFailsWithInvalidTitle(){
+
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidDueDate(){
+
+    }
+    @Test
+    public void creatingTaskItemSucceedsWithValidTitle(){
+
+    }
+    @Test
+    public void settingTaskItemDueDateFailsWithInvalidDate(){
+
+    }
+    @Test
+    public void settingTaskItemDueDateSucceedsWithValidDate(){
+
+    }
+    @Test
+    public void settingTaskItemTitleFailsWithInvalidTitle(){
+
+    }
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidTitle(){
+
+    }
+}
